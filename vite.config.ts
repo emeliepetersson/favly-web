9c12537ca3074fbbf775d93ee8042e0c9916f9a1
Emelie <emelie@vipmonkey.se>
2026-02-12 20:53:37 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const isGHPages = process.env.DEPLOY === "gh-pages";
+const basePath = isGHPages ? "/favly-web/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: basePath,
   server: {
     host: "::",
     port: 8080,
